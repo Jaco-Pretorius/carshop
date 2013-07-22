079928e569059e47d3ac1f1c43c2ce7c0f166338
Jaco Pretorius <jaco.pretorius@thoughtworks.com>
2013-07-22 14:12:27 -0400

    Adding spree example app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carstore::Application.config.secret_token = 'd5ec59b0db31fb3954b92554025f2a9b97fa48b2c3bf602c0d62e79f58e0a9ed7f387a0698f2b37b5705610c7292a77f0cd20c474ac5b1e9054ab85363dded9a'
